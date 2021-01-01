b78e7fffc1e522018e5f840234c26574c03e40ad
k-Oussama <oussama.khirat@univ-constantine2.dz>
2021-01-01 07:33:31 -0800

    features section is done !

--- src/components/features-section/features-section.component.jsx
+++ src/components/features-section/features-section.component.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import M_1 from "../../assets/img/m1.jpg";
-import M_2 from "../../assets/img/m2.png";
-import M_3 from "../../assets/img/m3.jpg";
-import M_4 from "../../assets/img/m4.jpg";
+import F_2 from "../../assets/img/f1.jpg";
+import F_1 from "../../assets/img/f2.jpg";
+import {Link} from "react-router-dom";
+
 
 
 
@@ -11,10 +11,30 @@ const FeaturesSection = () => {
 
     return (
         <div className="features-section">
-            <div className="bigTitle">
-                <span className="bigTitle__gold">World's</span> best <br/>
-                home <span className="bigTitle__gold">accessory</span>
-            </div>
+           <div className="content">
+               <div className="content__text">
+                   <p className="paragraph">welcome to smart web agency</p>
+                   <h1 className="title_1">
+                      We Are the best <br/>
+                      website agency in <br/>
+                      the world
+                   </h1>
+                   <p className="paragraph paragraph-2 ">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias atque eius magnam non quidem quod rem sapiente soluta suscipit ullam! Animi consectetur ea illo laboriosam pariatur quae repudiandae sint veritatis!</p>
+                   <div className="content__text--btn">
+                       <Link to=''>
+                           <i className="fas fa-arrow-right"/> Learn More
+                       </Link>
+                   </div>
+               </div>
+               <div className="content__imgBlock">
+                   <figure className="img img--1">
+                       <img src={F_1} alt="f1" className="img"  />
+                   </figure>
+                   <figure className="img img--2">
+                       <img src={F_2} alt="f2" className="img" />
+                   </figure>
+               </div>
+           </div>
         </div>
     );
 }
--- src/components/services-section/services-section.component.jsx
+++ src/components/services-section/services-section.component.jsx
@@ -1,41 +1,70 @@
-import React from 'react';
+import React , { useEffect, useState } from 'react';
 import Candle from '../../assets/img/candle.png';
 import Frame from '../../assets/img/picture-frame.png';
 import vase from '../../assets/img/vase.png';
 import Jars from '../../assets/img/jars.png';
 
 
-const ServicesSection = () => (
-    <section className="services-section">
-      <h1 className="title_2">services we are offering</h1>
-      <h1 className="title_1">
-          WE DESIGN DIGITAL PRODUCTS THAT <br/>
-          HELP GROW BUSINESSES
-      </h1>
-        <p className="paragraph" >we are committed to providing our customers with exceptional service <br/> while offering out employees the best training </p>
-      <div className="carts">
-          <div className="carts__cart">
-              <img src={Candle} alt="" className="carts__cart--img"/>
-              <h4 className="carts__cart--title" >Candles</h4>
-              <div className="red-divider"/>
-          </div>
-          <div className="carts__cart">
-              <img src={Frame} alt="" className="carts__cart--img"/>
-              <h4 className="carts__cart--title" >Frames</h4>
-              <div className="red-divider"/>
-          </div>
-          <div className="carts__cart">
-              <img src={vase} alt="" className="carts__cart--img"/>
-              <h4 className="carts__cart--title" >Vases</h4>
-              <div className="red-divider"/>
-          </div>
-          <div className="carts__cart">
-              <img src={Jars} alt="" className="carts__cart--img"/>
-              <h4 className="carts__cart--title" >Jars</h4>
-              <div className="red-divider"/>
-          </div>
-        </div>
-    </section>
-);
+const ServicesSection = () =>  {
+    const [isMobile, setIsMobile] = useState(window.innerWidth <=600);
+
+
+    useEffect(() => {
+        resize();
+        window.addEventListener("resize", () => {
+            resize();
+        });
+
+    },[]);
+
+    const resize = () => {
+        if (window.innerWidth <= 600 && isMobile === false) {
+            setIsMobile(true);
+            console.log("phone");
+        }
+
+        if (window.innerWidth <= 600 && isMobile === false) {
+            setIsMobile(true);
+            console.log("s");
+        }
+
+    }
+
+
+
+
+
+    return (
+        <section className="services-section">
+            <h1 className="title_2">services we are offering</h1>
+            <h1 className="title_1">
+                WE DESIGN DIGITAL PRODUCTS THAT <br/>
+                HELP GROW BUSINESSES
+            </h1>
+            <p className="paragraph" >we are committed to providing our customers with exceptional service <br/> while offering out employees the best training </p>
+            <div className="carts">
+                <div className="carts__cart">
+                    <img src={Candle} alt="" className="carts__cart--img"/>
+                    <h4 className="carts__cart--title" >Candles</h4>
+                    <div className="red-divider"/>
+                </div>
+                <div className="carts__cart">
+                    <img src={Frame} alt="" className="carts__cart--img"/>
+                    <h4 className="carts__cart--title" >Frames</h4>
+                    <div className="red-divider"/>
+                </div>
+                <div className="carts__cart">
+                    <img src={vase} alt="" className="carts__cart--img"/>
+                    <h4 className="carts__cart--title" >Vases</h4>
+                    <div className="red-divider"/>
+                </div>
+                <div className="carts__cart">
+                    <img src={Jars} alt="" className="carts__cart--img"/>
+                    <h4 className="carts__cart--title" >Jars</h4>
+                    <div className="red-divider"/>
+                </div>
+            </div>
+        </section>
+)};
 
 export default ServicesSection;
